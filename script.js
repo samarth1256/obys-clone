20eb9ec7ac94b5d4d0476c0f6d8784e72cf0dc7f
samarth1256 <samarthdua1256@gmail.com>
2023-12-17 03:33:14 +0530

    page1 done

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var tl=gsap.timeline()
+function loadingAnimation(){
+    var tl=gsap.timeline()
 tl.from(".line h1",{
     y:100,
     stagger:0.2,
@@ -35,8 +36,30 @@ tl.from("#page1",{
     opacity:0,
     delay:0.2,
     y:1600,
+    duration:0.6,
     ease:Power4,
 })
 tl.to("#loader",{
     display:"none",
 })
+tl.from("#nav",{
+    opacity:0
+})
+tl.from(".hero1 h1,.hero2 h1,.hero3 h2,.hero4 h1",{
+    y:120,
+    stagger:0.2
+})
+}
+loadingAnimation();
+
+function cursorAnimation(){
+    document.addEventListener('mousemove',function(dets){
+        gsap.to('#crsr',{
+            left:dets.x,
+            top:dets.y,
+        })
+    })
+    
+    Shery.makeMagnet("#nav-part2 h4");
+}
+cursorAnimation();
